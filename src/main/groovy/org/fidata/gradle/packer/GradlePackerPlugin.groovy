303dbd856961590c39a7fca4040c7620e833d87c
Basil Peace <grv87@yandex.ru>
2017-01-01 20:45:24 +0300

    legal: Update copyright year

--- src/main/groovy/org/fidata/gradle/packer/GradlePackerPlugin.groovy
+++ src/main/groovy/org/fidata/gradle/packer/GradlePackerPlugin.groovy
@@ -1,5 +1,5 @@
 /*	gradle-packer-plugin
-	Copyright © 2016  Basil Peace
+	Copyright © 2016-2017  Basil Peace
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
