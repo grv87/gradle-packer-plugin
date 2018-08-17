b2ce7cf2157a7ff9c8643ba0ab5f71a70b94ec2c
Basil Peace <grv87@yandex.ru>
2018-08-17 07:22:49 +0300

    WIP

--- src/main/groovy/org/fidata/gradle/packer/template/utils/PostProcessorArrayDefinition.groovy
+++ src/main/groovy/org/fidata/gradle/packer/template/utils/PostProcessorArrayDefinition.groovy
@@ -1,18 +1,12 @@
 package org.fidata.gradle.packer.template.utils
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.core.JsonProcessingException
-import com.fasterxml.jackson.core.JsonToken
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonDeserializer
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import groovy.transform.CompileStatic
 import org.fidata.gradle.packer.template.Context
 import org.fidata.gradle.packer.template.internal.InterpolableObject
 import org.gradle.api.tasks.Internal
 
-@JsonDeserialize(using = PostProcessorArrayDefinitionDeserializer)
 @CompileStatic
 class PostProcessorArrayDefinition extends InterpolableObject {
   static class ArrayClass extends ArrayList<PostProcessorDefinition> {}
@@ -20,17 +14,18 @@ class PostProcessorArrayDefinition extends InterpolableObject {
   @Internal
   Object rawValue
 
+  @JsonCreator
+  PostProcessorArrayDefinition(ArrayClass rawValue) {
+    this.rawValue
+  }
+
+  @JsonCreator
+  PostProcessorArrayDefinition(PostProcessorDefinition rawValue) {
+    this.rawValue
+  }
+
   @Override
   protected void doInterpolate(Context ctx) {
     // TODO
-  }
-
-  static class PostProcessorArrayDefinitionDeserializer extends JsonDeserializer<PostProcessorArrayDefinition> {
-    @Override
-    PostProcessorArrayDefinition deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-      return new PostProcessorArrayDefinition(
-        rawValue: jp.readValueAs(jp.currentToken() == JsonToken.START_ARRAY ? ArrayClass : PostProcessorDefinition)
-      )
-    }
   }
 }
--- src/main/groovy/org/fidata/gradle/packer/template/utils/PostProcessorDefinition.groovy
+++ src/main/groovy/org/fidata/gradle/packer/template/utils/PostProcessorDefinition.groovy
@@ -1,48 +1,31 @@
 package org.fidata.gradle.packer.template.utils
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
-import com.fasterxml.jackson.core.JsonParseException
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.core.JsonProcessingException
-import com.fasterxml.jackson.core.JsonToken
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonDeserializer
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import groovy.transform.CompileStatic
 import org.fidata.gradle.packer.template.Context
 import org.fidata.gradle.packer.template.PostProcessor
 import org.fidata.gradle.packer.template.internal.InterpolableObject
 import org.gradle.api.tasks.Internal
 
-@JsonDeserialize(using = PostProcessorsDefinitionDeserializer)
 @CompileStatic
 class PostProcessorDefinition extends InterpolableObject {
   @JsonValue
   @Internal
   Object rawValue
 
+  @JsonCreator
+  PostProcessorDefinition(String rawValue) {
+    this.rawValue = rawValue
+  }
+
+  @JsonCreator
+  PostProcessorDefinition(PostProcessor rawValue) {
+    this.rawValue = rawValue
+  }
+
   @Override
   protected void doInterpolate(Context ctx) {
     // TODO
-  }
-
-  static class PostProcessorsDefinitionDeserializer extends JsonDeserializer<PostProcessorDefinition> {
-    @Override
-    PostProcessorDefinition deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-      Class rawValueClass
-      switch (jp.currentToken()) {
-        case JsonToken.VALUE_STRING:
-          rawValueClass = String
-          break
-        case JsonToken.START_OBJECT:
-          rawValueClass = PostProcessor
-          break
-        default:
-          throw new JsonParseException(jp, 'post-processor: bad format')
-      }
-      return new PostProcessorDefinition(
-        rawValue: jp.readValueAs(rawValueClass)
-      )
-    }
   }
 }
